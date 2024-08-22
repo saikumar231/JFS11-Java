0ab5884a7f12c2eb31db120a8073f59a8d28ee3d
saikumar231 <sai20701a04e2@gmail.com>
2024-08-22 11:02:22 +0530

    Add files via upload

---
+++ localvariables.java
@@ -0,0 +1,9 @@
+//local variables : they will be stored in stack.
+public class localvariables{
+	public static void main(String[] args){
+		int n1=10;
+		int n2=20;
+		int sum=n1+n2;
+		System.out.println("The sum of"+n1+"and"+n2+"is"+sum);
+	}
+}
